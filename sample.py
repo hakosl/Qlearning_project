3cbb8e03cc8c4ec0a3f4a8bb91a2a5679ed966ff
Håkon Osland <hakon.ettestol.osland@gmail.com>
2019-03-20 13:43:06 +0800

    minor changes

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 '''
 This is a dummy file. Edit on it and send me a merge request so I can see if this works.
-Thanks.
+Thanks making minor changes.
 '''
 
-print('Hello World\n')
+print('Hello World!!\n')
