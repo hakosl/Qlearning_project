145e442e17e94364bd828aee702facaff65db854
Håkon Osland <hakon.ettestol.osland@gmail.com>
2019-04-11 17:02:04 +0800

    working on getting the q-learning algorithm working on the coinrun-env

--- dqn-tensorflow.py
+++ dqn-tensorflow.py
@@ -1,29 +1,64 @@
 import gym
+import numpy as np
+from coinrun import setup_utils, make, wrappers
+from baselines import deepq
 import tensorflow as tf
 
 
-env = gym.make('CartPole-v1')
-observation_size = env.observation_space.shape[0]
 
-def dense_nn(inputs, layer_sizes, scope_name):
-    with tf.variable_scope(scope_name):
-        for i, size in enumerate(layer_sizes):
-            inputs = tf.layers.dense(
-                inputs,
-                size,
-                activation=tf.nn.relu if i < len(layer_sizes) - 1 else None,
-                kernel_initializer=tf.contrib.layers.xavier_intializer(),
-                name=scope_name + '_l' + str(i)
-            )
-    return inputs
+def make_model():
+    return None
 
-batch_size = 32
+CHECKPOINT_NAME = "coinrun_agent.pkl"
 
-states = tf.placeholder(tf.float32, shape=(batch_size, observation_size), name="state")
-states_next = tf.placeholder(tf.float32, shape=(batch_size, observation_size), name="state_next")
-actions = tf.placeholder(tf.int32, shape=(batch_size,), name="action")
-rewards = tf.placeholder(tf.float32, shape=(batch_size,), name="reward")
-done_flags = tf.placeholder(tf.float32, shape=(batch_size,), name="done")
+def run_deepq(model, env, checkpoint=None, total_timesteps=100000):
+    statistics = dict(rewards=[], loss=[], timesteps=[])
 
-q = dense(states, [32, 32, 2], name="Q_primary")
-q_target = dense(states_next, [32, 32, 2], name="Q_target")
+    def callback(lcl, _glb):
+        statistics['rewards'].append(lcl['episode_rewards'])
+
+    
+    act = deepq.learn(
+        env,
+        network=model,
+        lr=1e-3,
+        total_timesteps=total_timesteps,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.02,
+        print_freq=10,
+        callback=callback,
+        #load_path=checkpoint
+    )
+
+    act.save(CHECKPOINT_NAME)
+
+    return statistics
+
+def main():
+
+    setup_utils.setup_and_load(use_cmd_line_args=False)
+
+    # Make the base enviroments that we will train the agent on first, this makes 1 gym enviroment
+    # But after each epoch a different enviroment will be choosen
+    base_env = make('standard', num_envs = 4)
+    base_env = wrappers.add_final_wrappers(base_env)
+    # Make the enviroment that we will attempt to transfer to
+    transfer_enviroment = make('standard', num_envs = 1)
+
+
+    with tf.Session():
+        model = make_model()
+
+        print("-----\ntraining base model on training enviroment\n-----")
+        base_statistics = run_deepq(model if model else 'cnn', base_env, total_timesteps=int(10e4))
+        print("-----\ntraining transfer model on test enviroment\n-----")
+        transfer_statistics = run_deepq(model if model else 'cnn', transfer_enviroment, checkpoint=CHEKPOINT_NAME, total_timesteps=int(10e4))
+        print("-----\ntraining transfer model on test enviroment\n-----")
+        transfer_enviroment_base_model_statistics = run_deepq(model if model else 'cnn', transfer_enviroment, checkpoint=CHEKPOINT_NAME, total_timesteps=int(10e4))
+
+
+
+
+if __name__ == '__main__':
+    main()
